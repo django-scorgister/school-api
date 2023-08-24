fe0e439029cc3a5596a6b4bc10e573c8f07d5173
Django-Argus <66014216+Django-Argus@users.noreply.github.com>
2023-08-24 17:10:34 +0200

    Patch version

--- src/net/argus/school/api/http/api/APIVersionHandler.java
+++ src/net/argus/school/api/http/api/APIVersionHandler.java
@@ -8,8 +8,6 @@ import net.argus.school.api.MainAPI;
 import net.argus.school.api.http.APIHandler;
 import net.argus.school.api.http.pack.PackagePrefab;
 import net.argus.system.UserSystem;
-import net.argus.util.DoubleStock;
-import net.argus.util.Version;
 
 
 public class APIVersionHandler extends APIHandler {
@@ -21,10 +19,8 @@ public class APIVersionHandler extends APIHandler {
 	@Override
 	public void doGet(HttpExchange exchange) throws IOException {
 		String version = MainAPI.VERSION.toString();
-		if(UserSystem.getUpdate() != null) {
-			DoubleStock<Version, Version> ds = UserSystem.getUpdate().getLatestVersion();
-			version = ds.getFirst().toString() + "." + ds.getSecond().toString();
-		}
+		if(UserSystem.getManifest() != null)
+			version = UserSystem.getCurrentVersion().toString() + "." + UserSystem.getCurrentDebug().toString();
 		
 		send(exchange, PackagePrefab.getVersionPackage(version));
 
